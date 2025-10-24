be69b95e5af4b0bf1e04ddfa2e0e822bf2087102
Briton Otieno <ojwangbriton@gmail.com>
2025-10-24 16:32:00 +0300

    Add migration to create organizations table with name, admin_user_id, and settings fields

---
+++ db/migrate/20251024132100_create_organizations.rb
@@ -0,0 +1,11 @@
+class CreateOrganizations < ActiveRecord::Migration[8.0]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.integer :admin_user_id
+      t.jsonb :settings
+
+      t.timestamps
+    end
+  end
+end
