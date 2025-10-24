57b42be531add00e6d900cc382855f74a3b031a9
Briton Otieno <ojwangbriton@gmail.com>
2025-10-24 16:34:37 +0300

    Add migration to create portfolios table with user and organization references

---
+++ db/migrate/20251024133254_create_portfolios.rb
@@ -0,0 +1,11 @@
+class CreatePortfolios < ActiveRecord::Migration[8.0]
+  def change
+    create_table :portfolios do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :organization, null: false, foreign_key: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
