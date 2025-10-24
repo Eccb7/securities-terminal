75bc4362f3ace6ac3393470963920a4342aba49c
Briton Otieno <ojwangbriton@gmail.com>
2025-10-24 16:43:59 +0300

    feat: Add validations and scopes to WatchlistItem model for improved functionality and data integrity

--- app/models/watchlist_item.rb
+++ app/models/watchlist_item.rb
@@ -1,4 +1,14 @@
 class WatchlistItem < ApplicationRecord
+  # Associations
   belongs_to :watchlist
   belongs_to :security
+
+  # Validations
+  validates :watchlist, presence: true
+  validates :security, presence: true
+  validates :security_id, uniqueness: { scope: :watchlist_id }
+
+  # Scopes
+  scope :for_watchlist, ->(watchlist_id) { where(watchlist_id: watchlist_id) }
+  scope :for_security, ->(security_id) { where(security_id: security_id) }
 end
