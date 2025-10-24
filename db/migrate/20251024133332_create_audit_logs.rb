c05fc08f7693473ea72eace07dac9e755da39595
Briton Otieno <ojwangbriton@gmail.com>
2025-10-24 16:37:28 +0300

    feat: Update audit_logs migration to enforce non-null constraints and set default values

--- db/migrate/20251024133332_create_audit_logs.rb
+++ db/migrate/20251024133332_create_audit_logs.rb
@@ -1,13 +1,16 @@
 class CreateAuditLogs < ActiveRecord::Migration[8.0]
   def change
     create_table :audit_logs do |t|
-      t.references :actor, null: false, foreign_key: true
-      t.string :action
+      t.references :actor, null: false, foreign_key: { to_table: :users }
+      t.string :action, null: false
       t.string :target_type
       t.integer :target_id
-      t.jsonb :payload
+      t.jsonb :payload, default: {}
 
       t.timestamps
     end
+
+    add_index :audit_logs, [ :target_type, :target_id ]
+    add_index :audit_logs, :created_at
   end
 end
